3052bea256920da5bdcb580f942911889e33f662
chadikoberssy <64798086+chadikoberssy@users.noreply.github.com>
2023-03-08 12:01:40 +0200

    Add files via upload

---
+++ App/js/script.js
@@ -0,0 +1,16 @@
+const overlay = $(".overlay");
+const navbar = $(".navbar-items");
+const body = $("body");
+const button = $(".navbar-toggler");
+
+button.click((e) => {
+    if(navbar.hasClass("open")){
+        overlay.css("opacity", "0");
+        body.removeClass("noscroll");
+        navbar.removeClass("open");
+    } else {
+        overlay.css("opacity", "0.5");
+        body.addClass("noscroll");
+        navbar.addClass("open");
+    }
+})
